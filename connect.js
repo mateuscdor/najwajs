5270ff9eac59b67edc0f646946df1eba743b083f
Glitch (glitch-hello-node) <none>
2022-03-14 15:45:06 +0000

    👡☺️ Checkpoint ./connect.js:27065786/4831

--- connect.js
+++ connect.js
@@ -1,6 +1,34 @@
 const axios = require("axios").default;
 const makeWASocket = require("@adiwajshing/baileys").default;
+const { DisconnectReason } = require("@adiwajshing/baileys");
+
+const connectToWhatsApp = () => {
+  const sock = makeWASocket({
+    // can provide additional config here
+    printQRInTerminal: false,
+  });
+
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut;
+      console.log(
+        "connection closed due to ",
+        lastDisconnect.error,
+        ", reconnecting ",
+        shouldReconnect
+      );
+      // reconnect if not logged out
+      if (shouldReconnect) {
+        connectToWhatsApp();
+      }
+    } else if (connection === "open") {
+      console.log("opened connection");
+    }
+  });
+};
 
 module.exports = () => {
-  
+  connectToWhatsApp();
 };
